681bd31c453805ee0dc8985dc369256ef3d6a79a
Huzaifa Haris <119068332+d3xture@users.noreply.github.com>
2024-02-29 11:56:13 +0500

    Update 01.py

--- 01.py
+++ 01.py
@@ -15,17 +15,3 @@ charge_q1 = float(input("Enter the charge of particle 1 (C): "))
 charge_q2 = calculate_charge_q2(force, distance_squared, charge_q1)
 
 print(f"The charge of particle 2 is: {charge_q2:.2e} C")
-
-//////////////////////
-
-F = float(input("Enter the value of F"))
-r_square = float(input("Enter the value of r"))
-K = float(input("Enter the value of K"))
-q1 = float(input("Enter the value of q1"))
-
-print("According to the problem number 32, we will using formula \n F = K q1q1/r^2")
-print("Since, we want to find q2, so q2 will be equal to Fr^2/kq1")
-
-q2 = (F * (r_square*2)) / (K * q1)
-
-print(f"so, the final answer will be {q2}")
